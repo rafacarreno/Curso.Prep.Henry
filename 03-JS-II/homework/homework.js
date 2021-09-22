640a9bb787e626c3831b21c3b982eb37018000a0
Rafael <rafacarreno93@gmail.com>
2021-09-22 10:20:19 -0300

    finish homework 3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -66,7 +66,7 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
-  // De lo contrario, devuelve el numero
+  // De lo contrario, devuelve el numero asd
 }
 
 function operadoresLogicos(num1, num2, num3) {
